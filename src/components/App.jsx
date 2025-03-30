b4a9c8bf0a4efa29f69222d27e150e12eedf9224
Daria Zharkova <dariazharkova1991@gmail.com>
2025-03-30 17:21:55 +0300

    create redux/contacts, redux/filters

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from '../redux/contacts/operations';
+import { useEffect } from 'react';
+import { selectError, selectLoading } from '../redux/contacts/selectors';
+
 export default function App() {
-  return <></>;
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    console.log('fetch');
+
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h1>Hello</h1>
+      {isLoading && !error && <b>Request in progress...</b>}
+      {!isLoading && error && <b>{error}</b>}
+    </>
+  );
 }
