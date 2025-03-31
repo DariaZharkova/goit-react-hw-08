4f5c97f9236e2e8ce07cfeeaefea6717cd343f6e
Daria Zharkova <dariazharkova1991@gmail.com>
2025-03-31 12:16:40 +0300

    add auth/operations; auth/slice; auth/selectors;

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,54 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchContacts } from '../redux/contacts/operations';
-import { useEffect } from 'react';
-import { selectError, selectLoading } from '../redux/contacts/selectors';
+import { register } from '../redux/auth/operations';
+// import { fetchContacts } from '../redux/contacts/operations';
+// import { useEffect } from 'react';
+// import { selectError, selectLoading } from '../redux/contacts/selectors';
 
 export default function App() {
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectLoading);
-  const error = useSelector(selectError);
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
 
-  useEffect(() => {
-    console.log('fetch');
+    dispatch(
+      register({
+        name: form.elements.name.value,
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+      })
+    );
 
-    dispatch(fetchContacts());
-  }, [dispatch]);
+    form.reset();
+  };
+  // const isLoading = useSelector(selectLoading);
+  // const error = useSelector(selectError);
+
+  // useEffect(() => {
+  //   console.log('fetch');
+
+  //   dispatch(fetchContacts());
+  // }, [dispatch]);
 
   return (
     <>
       <h1>Hello</h1>
-      {isLoading && !error && <b>Request in progress...</b>}
-      {!isLoading && error && <b>{error}</b>}
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <label>
+          Username
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Email
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Password
+          <input type="password" name="password" />
+        </label>
+        <button type="submit">Register</button>
+      </form>
+      {/* {isLoading && !error && <b>Request in progress...</b>}
+      {!isLoading && error && <b>{error}</b>} */}
     </>
   );
 }
