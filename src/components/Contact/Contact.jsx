1eb4cbc397e160466d1bd6a2f20cac49044b5403
Daria Zharkova <dariazharkova1991@gmail.com>
2025-04-01 21:08:10 +0300

    add contacts page

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,30 @@
+import { useDispatch } from 'react-redux';
+import { FaUser } from 'react-icons/fa';
+import { FaPhone } from 'react-icons/fa6';
+import { deleteContact } from '../../redux/contacts/operations';
+import css from './Contact.module.css';
+
+export default function Contact({ item }) {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteContact(item.id));
+  };
+
+  return (
+    <>
+      <div>
+        <p className={css.text}>
+          <FaUser className={css.icon} />
+          {item.name}
+        </p>
+        <p className={css.text}>
+          <FaPhone className={css.icon} /> {item.number}
+        </p>
+      </div>
+      <button className={css.btn} onClick={handleDelete}>
+        Delete
+      </button>
+    </>
+  );
+}
--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,7 +1,21 @@
+import { useSelector } from 'react-redux';
+import ContactForm from '../../components/ContactForm/ContactForm';
+import ContactList from '../../components/ContactList/ContactList';
+import SearchBox from '../../components/SearchBox/SearchBox';
+import { selectError, selectLoading } from '../../redux/contacts/selectors';
+
 export default function ContactsPage() {
+  const isLoading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
   return (
     <main>
       <h1>Contacts Page</h1>
+      <ContactForm />
+      <SearchBox />
+      {isLoading && !error && <b>Request in progress...</b>}
+      {!isLoading && error && <b>{error}</b>}
+      <ContactList />
     </main>
   );
 }
